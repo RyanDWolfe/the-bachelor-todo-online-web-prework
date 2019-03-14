79fa00832c13888587b10a1523ad08845ca406b2
Ryan Wolfe <ryan@studiohopfitness.com>
2019-03-14 18:04:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,5 @@
 def get_first_name_of_season_winner(data, season)
-  data[season][name].find {|status| status == "Winner"}
+  data[season].find {|status| status == "Winner"}
 end
 
 def get_contestant_name(data, occupation)
